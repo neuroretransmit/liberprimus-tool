056d540f5f0fddc95d518d085f37c669bf8f147d
null <null@null>
2024-04-28 19:24:34 -0700

    Added connection to database

--- db/db.py
+++ db/db.py
@@ -10,3 +10,14 @@ def insert_solution_attempt(scheme, key, shift, max_confidence, max_confidence_l
     conn.execute(f'INSERT INTO {TABLE_NAME} VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)',
                  (scheme, key, shift, max_confidence, max_confidence_lang, json.dumps(skips), json.dumps(excludes)))
     conn.commit()
+
+def solution_exists(scheme, key, shift, skips, excludes):
+    cursor = conn.cursor()
+    cursor.execute(f'SELECT * FROM {TABLE_NAME} WHERE scheme = ? AND key = ? AND shift = ? AND skips = ? AND excludes = ?',
+                 (scheme, key, shift, json.dumps(skips), json.dumps(excludes)))
+    # The UNIQUE constraint is specified by all these columns so no need for fetchall
+    data = cursor.fetchone()
+    if data is not None:
+        return True
+    return False
+
--- ga.py
+++ ga.py
@@ -1,5 +1,7 @@
+import json
 from specs import random_specs
 from random import getrandbits
+from db.db import insert_solution_attempt, solution_exists
 
 class GeneticAlgorithm:
     def __init__(self, target: float, initial_pool:list = []):
@@ -22,8 +24,18 @@ class GeneticAlgorithm:
             offspring = parents[0].crossover(**parents[1].__dict__)
             # TODO: finish mutation
             offspring.mutate()
+            # TODO: check if solution tried in database and continue if it has
+            scheme_name = getattr(offspring.crypto.scheme, '__name__', 'Unknown')
+            skips = json.dumps(offspring.crypto.skips) if offspring.crypto.skips is not None else None
+            excludes = json.dumps(offspring.crypto.excludes) if offspring.crypto.excludes is not None else None
+            if solution_exists(scheme_name, offspring.crypto.key, offspring.crypto.shift, skips, excludes):
+                continue
             # Run the spec and grab fitness
             offspring.rate()
+            # TODO: add solution to database
+            max_confidence = max([v for k, v in offspring.fitness.items()])
+            max_confidence_lang = next(k for k, v in offspring.fitness.items() if v == max_confidence)
+            insert_solution_attempt(scheme_name, offspring.crypto.key, offspring.crypto.shift, max_confidence, max_confidence_lang, skips, excludes)
             # Pop the worst individual out of the genepool in-place
             self.pool.pop()
             # Add new offspring
