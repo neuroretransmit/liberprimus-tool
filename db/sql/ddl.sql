3a10d6c07a6483d1cfeda71c33c448b92ce70779
null <null@null>
2024-04-29 10:20:09 -0700

    Switch DDL to postgres and add docker-compose.yml

--- db/sql/ddl.sql
+++ db/sql/ddl.sql
@@ -1,24 +1,11 @@
--- Optimizations for inserting things extremely fast
-
--- No transaction journal
-PRAGMA journal_mode = OFF;
--- Asynchronous
-PRAGMA synchronous = 0;
--- Specify cache size
-PRAGMA cache_size = 100000;
--- Modify locks
-PRAGMA locking_mode = EXCLUSIVE;
--- In-memory temp storet
-PRAGMA temp_store = MEMORY;
-
 CREATE TABLE solution_attempts (
-  id INT PRIMARY KEY,
+  id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
   scheme TEXT NOT NULL,
   key TEXT,
   shift INT,
   max_confidence REAL NOT NULL,
   max_confidence_lang TEXT,
-  skips JSON NOT NULL,
-  excludes JSON NOT NULL,
-  UNIQUE(scheme, key, shift, skips, excludes) ON CONFLICT IGNORE
+  skips JSONB,
+  excludes JSONB,
+  CONSTRAINT uniq_solution_attempts UNIQUE (scheme, key, shift, skips, excludes) 
 );
